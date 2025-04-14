3a4c405568f5264fd77ec2e640abffebf1e4cf27
chiji1108 <shingo.pad@gmail.com>
2025-04-14 19:22:33 +0900

    Update header z-index for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Menu } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex flex-col h-screen pt-20">
-      <header className="fixed top-0 left-0 right-0 flex justify-center">
+      <header className="fixed top-0 left-0 right-0 flex justify-center z-10">
         <div className="flex-1 flex justify-between items-center rounded-full bg-background border border-border px-4 py-2 max-w-screen-md m-4 shadow-xs">
           <h1 className="text-xl font-bold">Medical Shift</h1>
           <Button variant="ghost">
